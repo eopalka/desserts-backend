3164b198aa0de6db48aeec30b66af3ab2c829bd7
elisa <elisaopalka@gmail.com>
2021-03-06 02:00:41 -0500

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+author_1 = Author.create(name: "Elisa")
+author_2 = Author.create(name: "Fabian")
+
+Review.create([
+    { title: "Lava cake", comment: "Freaking yum", score: 8, author: author_1},
+    { title: "Postre de Natas", comment: "Delicioso", score: 6, author: author_2},
+    { title: "Tres Leches", comment: "Sabe buena", score: 10, author: author_1}
+])
